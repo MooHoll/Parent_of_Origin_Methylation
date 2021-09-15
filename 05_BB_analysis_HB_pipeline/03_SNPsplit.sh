2fa53ca22134df658cee3ebbcdf5a0520ad6ac34
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2021-09-15 15:22:12 +0100

    update

--- 05_BB_analysis_HB_pipeline/03_SNPsplit.sh
+++ 05_BB_analysis_HB_pipeline/03_SNPsplit.sh
@@ -1,3 +1,19 @@
+# Make SNP files for SNP split to work
+module load htslib/1.9
+bgzip -d *_final.vcf.gz
+
+for file in $(ls *_final.vcf)
+do
+    base=$(basename ${file} "_final.vcf")
+    grep -v '#' ${base}_final.vcf |
+    awk '{print $6 "\t" $1 "\t" $2 "\t" $3 "\t" $4 "/" $5}' - > ${base}_1.txt
+    echo -e "ID\tChr\tPosition\tSNP value\tRef/SNP" | cat - ${base}_1.txt > ${base}_snpsplit.txt
+done
+
+rm *1.txt
+
+#---------------------------------------------------------
+
 #!/bin/bash
 
 #PBS -N SNP_split
@@ -13,17 +29,52 @@ cd $PBS_O_WORKDIR
 # Load software needed
 module load samtools/1.9
 
+# Run snpsplit for each parental SNP set
+/scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite -o male_08 \
+--snp_file ../snps/m08_snpsplit.txt \
+trim_w08_1_bismark_bt2_pe.deduplicated.bam
+ 
+ /scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite -o queen_08 \
+--snp_file ../snps/q08_snpsplit.txt \
+trim_w08_1_bismark_bt2_pe.deduplicated.bam
+
+#---
+
 /scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
 --paired --bisulfite \
---snp_file <X> \
-<x>
+--snp_file ../snps/m19_snpsplit.txt \
+trim_w19_1_bismark_bt2_pe.deduplicated.bam
  
-for file in $(ls *.bam)
-do
-    /scratch/monoallelic/hjm31/bin_new/Bismark-0.22.3/deduplicate_bismark -p ${file}
-done
+ /scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite \
+--snp_file ../snps/q19_snpsplit.txt \
+trim_w19_1_bismark_bt2_pe.deduplicated.bam
+
+#---
+
+/scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite \
+--snp_file ../snps/m23_snpsplit.txt \
+trim_w23_1_bismark_bt2_pe.deduplicated.bam
  
+ /scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite \
+--snp_file ../snps/q23_snpsplit.txt \
+trim_w23_1_bismark_bt2_pe.deduplicated.bam
+
+#---
+
+/scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite \
+--snp_file ../snps/m37_snpsplit.txt \
+trim_w37_1_bismark_bt2_pe.deduplicated.bam
  
+ /scratch/monoallelic/hjm31/bin_new/SNPsplit-0.5.0/SNPsplit \
+--paired --bisulfite \
+--snp_file ../snps/q37_snpsplit.txt \
+trim_w37_1_bismark_bt2_pe.deduplicated.bam
  
  
  
